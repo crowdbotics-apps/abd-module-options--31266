fec74d46592e5674e15d2b7592fcb6bcacb7378b
Crowdbotics <team@crowdbotics.com>
2021-10-09 12:46:42 +0000

    Initial commit.

---
+++ backend/abd_module_options__31266/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for abd_module_options__31266 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abd_module_options__31266.settings')
+
+application = get_wsgi_application()
